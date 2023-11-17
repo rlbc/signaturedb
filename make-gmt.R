570e272decbde06fe62020aece27c1706cc83618
Rafael <rlbc@users.noreply.github.com>
2023-11-17 15:25:46 +0100

    changed all functions

--- make-gmt.R
+++ make-gmt.R
@@ -5,18 +5,16 @@ table <- readxl::read_xlsx(path = "SignatureDB_annotation_012422.xlsx")
 
 gmt_table <- table %>%
   group_by(Signature, `Signature category`) %>%
-  summarise("n_genes" = n(),
-            "Genes" = paste0(`Gene symbol`, collapse = "-")) %>%
-  separate_wider_delim(Genes, delim = "-",
-                       names = as.character(1:max(.$n_genes)),
-                       too_few = "align_start",
-                       too_many = "merge") %>%
-  select(-n_genes)
+  summarise("Genes" = paste0(`Gene symbol`, collapse = "-"))
 
-write.table(x = gmt_table,
-            file = "SignatureDB_annotation_012422.gmt",
-            quote = FALSE,
-            sep = "\t",
-            na = "",
-            row.names = FALSE,
-            col.names = FALSE)
+my_file <- file("SignatureDB_annotation_012422.gmt", open = "wa")
+for (i in 1:nrow(gmt_table)) {
+  sig <- as.character(gmt_table[i,1])
+  cat <- as.character(gmt_table[i,2])
+  genes <- as.character(gmt_table[i,3]) %>% strsplit(split = "-")
+  
+  line <- paste(sig, cat, paste0(genes[[1]], collapse = "\t"), sep = "\t")
+  
+  writeLines(line, con = my_file, sep = "\n")
+}
+close(my_file)
